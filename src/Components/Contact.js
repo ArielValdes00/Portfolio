91f9e801b666086174d385514968e316443262df
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2023-06-29 15:25:59 -0300

    actualizado archivo package.json

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
         setForm({ ...form, [e.target.name]: e.target.value });
     }
     return (
-        <div className='grid mx-5 items-center mt-10 pt-16' >
+        <div className='grid mx-5 items-center mt-10 pt-10' >
             <form onSubmit={handleSubmit} className="bg-neutral-800 p-6 rounded-md lg:w-2/3 lg:mx-auto" data-aos="fade-right" data-aos-duration="1000">
                 <div className='text-white text-center mb-10'>
                     <h2 className='text-5xl font-semibold'>Contact Me</h2>
