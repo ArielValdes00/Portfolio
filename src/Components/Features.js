9488d5fb223c7cb3901b8c35a3fe8528aecbc25d
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2023-08-11 19:51:58 -0300

    Se movió la etiqueta Head de la página Index.js al archivo _app.js

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -10,13 +10,13 @@ const Features = () => {
     }, []);
 
     return (
-        <div className="flex justify-center gap-4 md:gap-5 flex-wrap md:w-2/3 mx-auto px-1">
+        <div className="flex justify-center gap-4 md:gap-5 flex-wrap md:w-2/3 mx-auto px-5">
             {icons.map((icon, index) => (
-                <div key={index} className='flex flex-col gap-2 p-1' data-aos="fade-up" data-aos-delay={`${index * 100}`}>
-                    <span className='rounded-full p-2 bg-gray-200'>
-                        <img src={icon.image} width={55} height={30} alt={icon.name} className='p-1' />
+                <div key={index} className='flex flex-col items-center justify-center gap-2 p-1' data-aos="fade-up" data-aos-delay={`${index * 100}`}>
+                    <span className='rounded-full p-2 bg-gray-200 icons-img'>
+                        <img src={icon.image} alt={icon.name} className='p-1' />
                     </span>
-                    <p>{icon.name}</p>
+                    <p className='text-sm'>{icon.name}</p>
                 </div>
             ))}
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -67,20 +67,6 @@ export default function Index() {
 
     return (
         <>
-            <Head>
-                <title>Porfolio</title>
-                <link rel="icon" href="/logo-title.png" />
-                <meta name="description" content="Welcome to my Full Stack Developer portfolio. I am Ariel Valdés, a passionate Full Stack Developer with expertise in building web applications. Here you can explore some of my projects and works, showcasing my skills in front-end and back-end development, as well as database management. Feel free to browse around and get in touch if you're interested in collaborating on exciting projects!" />
-                <meta property="og:title" content="Full Stack Developer Portfolio - Ariel Valdés" />
-                <meta property="og:description" content="Welcome to my Full Stack Developer portfolio. I am Ariel Valdés, a passionate Full Stack Developer with expertise in building web applications. Here you can explore some of my projects and works, showcasing my skills in front-end and back-end development, as well as database management. Feel free to browse around and get in touch if you're interested in collaborating on exciting projects!" />
-                <meta property="og:image" content="https://res.cloudinary.com/dnczjmsbt/image/upload/v1691437299/bg-portfolio_f0unhu.png" />
-                <meta property="og:url" content="https://portfolio-ochre-six-55.vercel.app/" />
-                <meta property="og:type" content="website" />
-                <meta property="og:locale" content="en_US" />
-                <meta property="og:site_name" content="Full Stack Developer Portfolio - Ariel Valdés" />
-                <meta name="keywords" content="full stack developer, web development, front-end, back-end, database management, projects, works, react, next.js, tailwind, javascript" />
-                <meta name="author" content="Ariel Valdés" />
-            </Head>
             <main className="flex flex-col h-screen ">
                 {showNavbar && (
                     <NavBar showMenu={showMenu}
@@ -113,7 +99,7 @@ export default function Index() {
                 </div>
             </section>
             <section className='h-screen grid place-items-center mx-auto' ref={sectionFeatures}>
-                <div className='text-neutral-100 text-center'>
+                <div className='text-neutral-100 text-center px-5'>
                     <div className='mb-10 text-white' data-aos="fade-up" data-aos-delay="50">
                         <h2 className='text-5xl font-semibold'>Features</h2>
                     </div>
@@ -124,7 +110,7 @@ export default function Index() {
                 <Projects />
             </section>
             <section className='h-screen flex flex-col lg:gap-[130px] lg:flex-row justify-center items-center overflow-x-hidden' data-aos="fade-right" data-aos-duration="1000" ref={sectionContact} >
-                <GetInTouch toast={toast}/>
+                <GetInTouch toast={toast} />
                 <Contact toast={toast} />
             </section>
             <ToastContainer
